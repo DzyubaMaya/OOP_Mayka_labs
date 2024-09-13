d5900bd3739de48861be94f27f339df8d1e2bcfd
Dzyuba Maya <dzyubamaiya@yandex.ru>
2024-09-13 20:00:18 +0000

    добавление обработки неправильного ввода данных

--- lab01/1lab.cpp
+++ lab01/1lab.cpp
@@ -6,17 +6,46 @@
 int main() {
     int hour, minute;
     std::string period;
+    std::string resultatik;
 
-    // Ввод данных пользователем
-    std::cout << "Введите час (1-12): ";
-    std::cin >> hour;
-    std::cout << "Введите минуты (0-59): ";
-    std::cin >> minute;
-    std::cout << "Введите период (am/pm): ";
-    std::cin >> period;
+    // Ввод данных пользователем для часов
+    while (true) {
+        std::cout << "Введите час (1-12): ";
+        std::cin >> hour;
+        //проверяем часики
+        resultatik = perevodik(hour, 0, "am"); 
+        if (resultatik.find("Ошибочка") == std::string::npos) {
+            break;
+        }
+        std::cout << resultatik << std::endl;
+    }
+
+    // Ввод данных пользователем для минут
+    while (true) {
+        std::cout << "Введите минуты (0-59): ";
+        std::cin >> minute;
+        //проверяем минуты
+        resultatik = perevodik(1, minute, "am"); 
+        if (resultatik.find("Ошибочка") == std::string::npos) {
+            break;
+        }
+        std::cout << resultatik << std::endl;
+    }
+
+    // Ввод данных пользователем для периода
+    while (true) {
+        std::cout << "Введите период (am/pm): ";
+        std::cin >> period;
+        //проверяем период
+        resultatik = perevodik(1, 0, period); 
+        if (resultatik.find("Ошибочка") == std::string::npos) {
+            break;
+        }
+        std::cout << resultatik << std::endl;
+    }
 
     // Преобразование и вывод результата
-    std::string resultatik = perevodik(hour, minute, period);
+    resultatik = perevodik(hour, minute, period);
     std::cout << "Время в 24-часовом формате: " << resultatik << std::endl;
 
     return 0;
